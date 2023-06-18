3f523b88f8dc614b3ea9b856c606cb2b9ea401e9
unknown <ihigagerald@gmail.com>
2023-06-18 20:10:22 +0300

    Added a guard to prevent unauthenticated users from accessing the dashboard or other routes on the dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {MovieslistComponent} from "./pages/dashboard/components/movieslist/movieslist.component";
 import {SearchmoviesComponent} from "./pages/dashboard/components/searchmovies/searchmovies.component";
 import {SuggestmoviesComponent} from "./pages/dashboard/components/suggestmovies/suggestmovies.component";
+import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   {path:'home', component:LandingComponent},
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent},
-  {path:'latest-movies', component:MovieslistComponent},
-  {path:'search-movies', component:SearchmoviesComponent},
-  {path:'suggest-movies', component:SuggestmoviesComponent}
+  {path:'dashboard', component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'latest-movies', component:MovieslistComponent, canActivate:[AuthGuard]},
+  {path:'search-movies', component:SearchmoviesComponent, canActivate:[AuthGuard]},
+  {path:'suggest-movies', component:SuggestmoviesComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router:Router) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = localStorage.getItem('email')
+    if (user && user!=='undefined'){
+      // if there's a user, the route can be activated
+      return true;
+    } else {
+      // if no user, redirect to the homepage
+      this.router.navigate(['/home'])
+      return false
+    }
+
+  }
+
+}
